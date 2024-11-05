31f3801113b6c9b9219245da4d43ff7e909354df
Sam Wilson <sam@binarycake.ca>
2024-11-05 17:18:27 -0500

    Get test coverage to 100%; finish moving etherum-rlp

--- src/ethereum_rlp/rlp.py
+++ src/ethereum_rlp/rlp.py
@@ -1,24 +1,13 @@
 """
-.. _rlp:
-
-Recursive Length Prefix (RLP) Encoding
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-.. contents:: Table of Contents
-    :backlinks: none
-    :local:
-
-Introduction
-------------
-
 Defines the serialization and deserialization format used throughout Ethereum.
 """
 
-from dataclasses import astuple, fields, is_dataclass
+from dataclasses import Field, astuple, fields, is_dataclass
 from typing import (
     Any,
     ClassVar,
     Dict,
+    List,
     Protocol,
     Sequence,
     Tuple,
@@ -36,8 +25,7 @@ from typing import (
 from ethereum_types.bytes import Bytes, FixedBytes
 from ethereum_types.numeric import FixedUnsigned, Uint
 
-from ethereum.crypto.hash import Hash32, keccak256
-from ethereum.exceptions import RLPDecodingError, RLPEncodingError
+from .exceptions import DecodingError, EncodingError
 
 
 class RLP(Protocol):
@@ -47,7 +35,7 @@ class RLP(Protocol):
     [`Protocol`]: https://docs.python.org/3/library/typing.html#typing.Protocol
     """
 
-    __dataclass_fields__: ClassVar[Dict]
+    __dataclass_fields__: ClassVar[Dict[str, Field[object]]]
 
 
 Simple: TypeAlias = Union[Sequence["Simple"], bytes]
@@ -65,17 +53,6 @@ Extended: TypeAlias = Union[
 def encode(raw_data: Extended) -> Bytes:
     """
     Encodes `raw_data` into a sequence of bytes using RLP.
-
-    Parameters
-    ----------
-    raw_data :
-        A `Bytes`, `Uint`, `Uint256` or sequence of `RLP` encodable
-        objects.
-
-    Returns
-    -------
-    encoded : `ethereum.base_types.Bytes`
-        The RLP encoded bytes representing `raw_data`.
     """
     if isinstance(raw_data, Sequence):
         if isinstance(raw_data, (bytearray, bytes)):
@@ -94,7 +71,7 @@ def encode(raw_data: Extended) -> Bytes:
     elif is_dataclass(raw_data):
         return encode(astuple(raw_data))
     else:
-        raise RLPEncodingError(
+        raise EncodingError(
             "RLP Encoding of type {} is not supported".format(type(raw_data))
         )
 
@@ -102,16 +79,6 @@ def encode(raw_data: Extended) -> Bytes:
 def encode_bytes(raw_bytes: Bytes) -> Bytes:
     """
     Encodes `raw_bytes`, a sequence of bytes, using RLP.
-
-    Parameters
-    ----------
-    raw_bytes :
-        Bytes to encode with RLP.
-
-    Returns
-    -------
-    encoded : `ethereum.base_types.Bytes`
-        The RLP encoded bytes representing `raw_bytes`.
     """
     len_raw_data = len(raw_bytes)
 
@@ -132,18 +99,8 @@ def encode_bytes(raw_bytes: Bytes) -> Bytes:
 def encode_sequence(raw_sequence: Sequence[Extended]) -> Bytes:
     """
     Encodes a list of RLP encodable objects (`raw_sequence`) using RLP.
-
-    Parameters
-    ----------
-    raw_sequence :
-            Sequence of RLP encodable objects.
-
-    Returns
-    -------
-    encoded : `ethereum.base_types.Bytes`
-        The RLP encoded bytes representing `raw_sequence`.
     """
-    joined_encodings = get_joined_encodings(raw_sequence)
+    joined_encodings = join_encodings(raw_sequence)
     len_joined_encodings = len(joined_encodings)
 
     if len_joined_encodings < 0x38:
@@ -157,21 +114,10 @@ def encode_sequence(raw_sequence: Sequence[Extended]) -> Bytes:
         )
 
 
-def get_joined_encodings(raw_sequence: Sequence[Extended]) -> Bytes:
+def join_encodings(raw_sequence: Sequence[Extended]) -> Bytes:
     """
     Obtain concatenation of rlp encoding for each item in the sequence
     raw_sequence.
-
-    Parameters
-    ----------
-    raw_sequence :
-        Sequence to encode with RLP.
-
-    Returns
-    -------
-    joined_encodings : `ethereum.base_types.Bytes`
-        The concatenated RLP encoded bytes for each item in sequence
-        raw_sequence.
     """
     return b"".join(encode(item) for item in raw_sequence)
 
@@ -185,19 +131,9 @@ def decode(encoded_data: Bytes) -> Simple:
     """
     Decodes an integer, byte sequence, or list of RLP encodable objects
     from the byte sequence `encoded_data`, using RLP.
-
-    Parameters
-    ----------
-    encoded_data :
-        A sequence of bytes, in RLP form.
-
-    Returns
-    -------
-    decoded_data : `RLP`
-        Object decoded from `encoded_data`.
     """
     if len(encoded_data) <= 0:
-        raise RLPDecodingError("Cannot decode empty bytestring")
+        raise DecodingError("Cannot decode empty bytestring")
 
     if encoded_data[0] <= 0xBF:
         # This means that the raw data is of type bytes
@@ -214,18 +150,6 @@ def decode_to(cls: Type[U], encoded_data: Bytes) -> U:
     """
     Decode the bytes in `encoded_data` to an object of type `cls`. `cls` can be
     a `Bytes` subclass, a dataclass, `Uint`, `U256` or `Tuple[cls]`.
-
-    Parameters
-    ----------
-    cls: `Type[U]`
-        The type to decode to.
-    encoded_data :
-        A sequence of bytes, in RLP form.
-
-    Returns
-    -------
-    decoded_data : `U`
-        Object decoded from `encoded_data`.
     """
     decoded = decode(encoded_data)
     return _deserialize_to(cls, decoded)
@@ -233,12 +157,12 @@ def decode_to(cls: Type[U], encoded_data: Bytes) -> U:
 
 @overload
 def _deserialize_to(class_: Type[U], value: Simple) -> U:
-    pass
+    pass  # pragma: no cover
 
 
 @overload
 def _deserialize_to(class_: object, value: Simple) -> Extended:
-    pass
+    pass  # pragma: no cover
 
 
 def _deserialize_to(class_: object, value: Simple) -> Extended:
@@ -262,13 +186,13 @@ def _deserialize_to_dataclass(cls: Type[U], decoded: Simple) -> U:
     target_fields = fields(cls)
 
     if isinstance(decoded, bytes):
-        raise RLPDecodingError(f"got `bytes` while decoding `{cls.__name__}`")
+        raise DecodingError(f"got `bytes` while decoding `{cls.__name__}`")
 
     if len(target_fields) != len(decoded):
         name = cls.__name__
         actual = len(decoded)
         expected = len(target_fields)
-        raise RLPDecodingError(
+        raise DecodingError(
             f"`{name}` needs {expected} field(s), but got {actual} instead"
         )
 
@@ -289,29 +213,29 @@ def _deserialize_to_bool(value: Simple) -> bool:
     elif value == b"\x01":
         return True
     else:
-        raise RLPDecodingError
+        raise DecodingError("invalid boolean")
 
 
 def _deserialize_to_bytes(
     class_: Union[Type[Bytes], Type[FixedBytes]], value: Simple
 ) -> Union[Bytes, FixedBytes]:
     if not isinstance(value, bytes):
-        raise RLPDecodingError
+        raise DecodingError("invalid bytes")
     try:
         return class_(value)
     except ValueError as e:
-        raise RLPDecodingError from e
+        raise DecodingError from e
 
 
 def _deserialize_to_uint(
     class_: Union[Type[Uint], Type[FixedUnsigned]], decoded: Simple
 ) -> Union[Uint, FixedUnsigned]:
     if not isinstance(decoded, bytes):
-        raise RLPDecodingError
+        raise DecodingError("invalid uint")
     try:
         return class_.from_be_bytes(decoded)
     except ValueError as e:
-        raise RLPDecodingError from e
+        raise DecodingError from e
 
 
 def _deserialize_to_annotation(annotation: object, value: Simple) -> Extended:
@@ -320,6 +244,8 @@ def _deserialize_to_annotation(annotation: object, value: Simple) -> Extended:
         return _deserialize_to_union(annotation, value)
     elif origin in (Tuple, tuple):
         return _deserialize_to_tuple(annotation, value)
+    elif origin in (List, Sequence, list):
+        return _deserialize_to_list(annotation, value)
     elif origin is None:
         raise Exception(annotation)
     else:
@@ -328,7 +254,7 @@ def _deserialize_to_annotation(annotation: object, value: Simple) -> Extended:
 
 def _deserialize_to_union(annotation: object, value: Simple) -> Extended:
     arguments = get_args(annotation)
-    successes = []
+    successes: List[Extended] = []
     failures = []
     for argument in arguments:
         try:
@@ -342,16 +268,16 @@ def _deserialize_to_union(annotation: object, value: Simple) -> Extended:
     if len(successes) == 1:
         return successes[0]
     elif not successes:
-        raise RLPDecodingError(f"no matching union variant\n{failures!r}")
+        raise DecodingError(f"no matching union variant\n{failures!r}")
     else:
-        raise RLPDecodingError("multiple matching union variants")
+        raise DecodingError("multiple matching union variants")
 
 
 def _deserialize_to_tuple(
     annotation: object, values: Simple
 ) -> Sequence[Extended]:
     if isinstance(values, bytes):
-        raise RLPDecodingError
+        raise DecodingError("invalid tuple")
     arguments = list(get_args(annotation))
 
     if arguments[-1] is Ellipsis:
@@ -366,47 +292,46 @@ def _deserialize_to_tuple(
     return tuple(decoded)
 
 
+def _deserialize_to_list(
+    annotation: object, values: Simple
+) -> Sequence[Extended]:
+    if isinstance(values, bytes):
+        raise DecodingError("invalid list")
+    argument = get_args(annotation)[0]
+    return [_deserialize_to(argument, v) for v in values]
+
+
 def decode_to_bytes(encoded_bytes: Bytes) -> Bytes:
     """
     Decodes a rlp encoded byte stream assuming that the decoded data
     should be of type `bytes`.
-
-    Parameters
-    ----------
-    encoded_bytes :
-        RLP encoded byte stream.
-
-    Returns
-    -------
-    decoded : `ethereum.base_types.Bytes`
-        RLP decoded Bytes data
     """
     if len(encoded_bytes) == 1 and encoded_bytes[0] < 0x80:
         return encoded_bytes
     elif encoded_bytes[0] <= 0xB7:
         len_raw_data = encoded_bytes[0] - 0x80
         if len_raw_data >= len(encoded_bytes):
-            raise RLPDecodingError
+            raise DecodingError("truncated")
         raw_data = encoded_bytes[1 : 1 + len_raw_data]
         if len_raw_data == 1 and raw_data[0] < 0x80:
-            raise RLPDecodingError
+            raise DecodingError
         return raw_data
     else:
         # This is the index in the encoded data at which decoded data
         # starts from.
         decoded_data_start_idx = 1 + encoded_bytes[0] - 0xB7
         if decoded_data_start_idx - 1 >= len(encoded_bytes):
-            raise RLPDecodingError
+            raise DecodingError
         if encoded_bytes[1] == 0:
-            raise RLPDecodingError
+            raise DecodingError
         len_decoded_data = int(
             Uint.from_be_bytes(encoded_bytes[1:decoded_data_start_idx])
         )
         if len_decoded_data < 0x38:
-            raise RLPDecodingError
+            raise DecodingError
         decoded_data_end_idx = decoded_data_start_idx + int(len_decoded_data)
         if decoded_data_end_idx - 1 >= len(encoded_bytes):
-            raise RLPDecodingError
+            raise DecodingError
         return encoded_bytes[decoded_data_start_idx:decoded_data_end_idx]
 
 
@@ -414,38 +339,28 @@ def decode_to_sequence(encoded_sequence: Bytes) -> Sequence[Simple]:
     """
     Decodes a rlp encoded byte stream assuming that the decoded data
     should be of type `Sequence` of objects.
-
-    Parameters
-    ----------
-    encoded_sequence :
-        An RLP encoded Sequence.
-
-    Returns
-    -------
-    decoded : `Sequence[RLP]`
-        Sequence of objects decoded from `encoded_sequence`.
     """
     if encoded_sequence[0] <= 0xF7:
         len_joined_encodings = encoded_sequence[0] - 0xC0
         if len_joined_encodings >= len(encoded_sequence):
-            raise RLPDecodingError
+            raise DecodingError
         joined_encodings = encoded_sequence[1 : 1 + len_joined_encodings]
     else:
         joined_encodings_start_idx = 1 + encoded_sequence[0] - 0xF7
         if joined_encodings_start_idx - 1 >= len(encoded_sequence):
-            raise RLPDecodingError
+            raise DecodingError
         if encoded_sequence[1] == 0:
-            raise RLPDecodingError
+            raise DecodingError
         len_joined_encodings = int(
             Uint.from_be_bytes(encoded_sequence[1:joined_encodings_start_idx])
         )
         if len_joined_encodings < 0x38:
-            raise RLPDecodingError
+            raise DecodingError
         joined_encodings_end_idx = (
             joined_encodings_start_idx + len_joined_encodings
         )
         if joined_encodings_end_idx - 1 >= len(encoded_sequence):
-            raise RLPDecodingError
+            raise DecodingError
         joined_encodings = encoded_sequence[
             joined_encodings_start_idx:joined_encodings_end_idx
         ]
@@ -457,16 +372,6 @@ def decode_joined_encodings(joined_encodings: Bytes) -> Sequence[Simple]:
     """
     Decodes `joined_encodings`, which is a concatenation of RLP encoded
     objects.
-
-    Parameters
-    ----------
-    joined_encodings :
-        concatenation of RLP encoded objects
-
-    Returns
-    -------
-    decoded : `List[RLP]`
-        A list of objects decoded from `joined_encodings`.
     """
     decoded_sequence = []
 
@@ -476,7 +381,7 @@ def decode_joined_encodings(joined_encodings: Bytes) -> Sequence[Simple]:
             joined_encodings[item_start_idx:]
         )
         if item_start_idx + encoded_item_length - 1 >= len(joined_encodings):
-            raise RLPDecodingError
+            raise DecodingError
         encoded_item = joined_encodings[
             item_start_idx : item_start_idx + encoded_item_length
         ]
@@ -490,24 +395,12 @@ def decode_item_length(encoded_data: Bytes) -> int:
     """
     Find the length of the rlp encoding for the first object in the
     encoded sequence.
+
     Here `encoded_data` refers to concatenation of rlp encoding for each
     item in a sequence.
-
-    NOTE - This is a helper function not described in the spec. It was
-    introduced as the spec doesn't discuss about decoding the RLP encoded
-    data.
-
-    Parameters
-    ----------
-    encoded_data :
-        RLP encoded data for a sequence of objects.
-
-    Returns
-    -------
-    rlp_length : `int`
     """
     if len(encoded_data) <= 0:
-        raise RLPDecodingError
+        raise DecodingError
 
     first_rlp_byte = encoded_data[0]
 
@@ -531,9 +424,9 @@ def decode_item_length(encoded_data: Bytes) -> int:
     elif first_rlp_byte <= 0xBF:
         length_length = first_rlp_byte - 0xB7
         if length_length >= len(encoded_data):
-            raise RLPDecodingError
+            raise DecodingError
         if encoded_data[1] == 0:
-            raise RLPDecodingError
+            raise DecodingError
         decoded_data_length = int(
             Uint.from_be_bytes(encoded_data[1 : 1 + length_length])
         )
@@ -546,28 +439,11 @@ def decode_item_length(encoded_data: Bytes) -> int:
     elif first_rlp_byte <= 0xFF:
         length_length = first_rlp_byte - 0xF7
         if length_length >= len(encoded_data):
-            raise RLPDecodingError
+            raise DecodingError
         if encoded_data[1] == 0:
-            raise RLPDecodingError
+            raise DecodingError
         decoded_data_length = int(
             Uint.from_be_bytes(encoded_data[1 : 1 + length_length])
         )
 
     return 1 + length_length + decoded_data_length
-
-
-def rlp_hash(data: Extended) -> Hash32:
-    """
-    Obtain the keccak-256 hash of the rlp encoding of the passed in data.
-
-    Parameters
-    ----------
-    data :
-        The data for which we need the rlp hash.
-
-    Returns
-    -------
-    hash : `Hash32`
-        The rlp hash of the passed in data.
-    """
-    return keccak256(encode(data))
